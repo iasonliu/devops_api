350aec86f4926f569ff8de7e9335b661946b305d
xiangxu <iasonliu@gmail.com>
2018-10-09 16:44:49 +0800

    initial commit

---
+++ lib/devops_api_web/router.ex
@@ -0,0 +1,11 @@
+defmodule DevopsApiWeb.Router do
+  use DevopsApiWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", DevopsApiWeb do
+    pipe_through :api
+  end
+end
